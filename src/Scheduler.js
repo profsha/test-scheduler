5d460d8a7c5c62d8c62c3ab0b0aba79003da5936
Aleksandr Zakharov <profsha007@gmail.com>
2022-08-12 18:49:41 +0300

    with ref

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,23 +1,37 @@
 import {DayPilotScheduler} from "daypilot-pro-react";
 import EmptyResource from './EmptyResource';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useRef } from 'react';
 
 function Scheduler(props) {
   
   const [cellWidth, setCellWidth] = useState(40);
+
+  const ref = useRef(null);
 
   const incrementCellWidth = useCallback(() => {
     setCellWidth((currentWidth) => {
       return currentWidth+10;
     })
   }, [setCellWidth]);
+
+  
+  const incrementCellWidthREF = useCallback(() => {
+    if (ref.current) {
+      ref.current.control.cellWidth = ref.current.control.cellWidth + 10;
+      ref.current.control.update();
+    }
+  }, []);
+
+
   const handleBeforeRowHeaderDomAdd = useCallback((args) => {
     args.element = <EmptyResource />
   }, []);
 
     return (<>
       <button onClick={incrementCellWidth}>Set cell width</button>
+      <button onClick={incrementCellWidthREF}>Set cell width REF</button>
       <DayPilotScheduler
+      ref={ref}
                 resources = {[
                 {name: "Resource A", id: "A"},
                 {name: "Resource B", id: "B"},
